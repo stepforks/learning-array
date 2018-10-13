376800a46b3fafbd0c9592b0fa6ff5cd1f478f2f
lucky <44020019+luckyganesh@users.noreply.github.com>
2018-10-13 11:13:50 +0530

    added feature of selecting even numbers
    features:
    1.selecting odd numbers
    2.selecting even numbers

--- library.js
+++ library.js
@@ -7,4 +7,16 @@ const selectOddNumbers = function(numbers){
   }
   return oddNumbers;
 }
+
+const selectEvenNumbers = function(numbers){
+  let evenNumbers=[];
+  for(let number of numbers){
+    if(!(number%2)){
+      evenNumbers.push(number);
+    }
+  }
+  return evenNumbers;
+}
+
 exports.selectOddNumbers = selectOddNumbers;
+exports.selectEvenNumbers = selectEvenNumbers;
--- test_files.js
+++ test_files.js
@@ -1,5 +1,8 @@
 const assert = require("assert");
 const selectOddNumbers = require("./library.js").selectOddNumbers;
+const selectEvenNumbers = require("./library.js").selectEvenNumbers;
 const numbers=require("./input_file.js").numbers;
 assert.deepEqual(selectOddNumbers(numbers),[1,3,5,7,9]);
+console.log("test passed");
+assert.deepEqual(selectEvenNumbers(numbers),[2,4,6,8]);
 console.log("test passed");
